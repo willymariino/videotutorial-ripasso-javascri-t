a2bb8bd84b54372494522a630f458f500e2baac7
willymariino <wg.mariino@gmail.com>
2025-03-13 01:00:23 +0100

    ic cicli

---
+++ i cicli/cicli.js
@@ -0,0 +1,9 @@
+let arrayy = [23, 13, 24, 25, 99]
+
+// non conoscendo i loop dovrei fare 
+
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
