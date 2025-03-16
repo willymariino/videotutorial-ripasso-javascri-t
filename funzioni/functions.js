b200a1b46db034421aa665c110012a8079a1c85a
willymariino <wg.mariino@gmail.com>
2025-03-16 23:12:29 +0100

    corretta una piccola cosa nei commenti nei cicli

---
+++ funzioni/functions.js
@@ -0,0 +1,8 @@
+// la funzione è quella cosa che ci permentte di ripetere la stessa parte di codice più volte nel programma
+// praticamente un insieme di azioni che vogliamo far fare al programma ogni volta che diciamo "fai quello"
+
+function cucinaPasta() {
+
+
+
+}
